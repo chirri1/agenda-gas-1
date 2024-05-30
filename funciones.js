70f798bcc0ec6184ea510bdf74c6a2ea714befcf
Gustavo <gustavo6276@gmail.com>
2024-05-30 19:32:27 +0200

    video9 curso gas-avanzado

--- funciones.js
+++ funciones.js
@@ -2,3 +2,15 @@ function doGet()
 {
     return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google Apps Script');
 }
+
+function obtenerDatosHTML(nombre)
+{
+    return HtmlService.createHtmlOutputFromFile(nombre).getContent();
+}
+
+function obtenerContactos()
+{
+    let hoja = SpreadsheetApp.openById('1rWa_QRZAFIU3sgZA4uHGEHT154XoV0kmGlKadtvTrO0').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+}
